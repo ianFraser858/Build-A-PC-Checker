4d82067494ecf441728c7c2271e73fb4191aaf21
Ian Fraser <33810966+ianFraser858@users.noreply.github.com>
2019-08-29 15:04:57 -0500

    Initial commit

---
+++ redditCheck.py
@@ -0,0 +1,32 @@
+import praw
+import re
+
+from twilio.rest import Client
+
+try:
+    from .local_settings import *
+except ImportError:
+    # need to add error handling here
+    pass
+
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     user_agent=user_agent)
+
+client = Client(twilio_sid, twilio_auth)
+
+subreddit = reddit.subreddit('buildapcsales')
+
+sought_gpu = ['2070', '2060', '2080']
+
+for submission in subreddit.new(limit=25):
+    title = submission.title
+    if(title[title.find('[')+1: title.find(']')]) == 'GPU' and any(entry in title for entry in sought_gpu):
+        all_nums = re.findall(r"\d*\.\d+|\d+", title)
+        filtered_nums = [entry for entry in all_nums if 250 < float(entry) < 450]
+        print(filtered_nums)
+
+
+client.messages.create(to='+12565428524',
+                       from_='+12056354084',
+                       body='Hello from Python!')
