2a7eba4aac04da204994f1d906372221007cffdb
Ian Fraser <icf0001@auburn.edu>
2020-05-30 13:09:34 -0500

    testing cron update

--- redditCheck.py
+++ redditCheck.py
@@ -12,11 +12,11 @@ reddit = praw.Reddit(client_id=client_id,
                      user_agent=user_agent)
 client = Client(twilio_sid, twilio_auth)
 subreddit = reddit.subreddit('buildapcsales')
-sought_gpu = ['1660ti', '1660TI', '2060', '2070']
+sought_gpu = ['1660ti', '1660TI', '2060', '2070', 'ASUS']
 
 for submission in subreddit.new(limit=3):
     title = submission.title
-    if(title[title.find('[')+1: title.find(']')]) == 'GPU' \
+    if(title[title.find('[')+1: title.find(']')]) == 'MONITOR' \
             and any(entry in title for entry in sought_gpu)\
             and (datetime.utcnow() - datetime.utcfromtimestamp(submission.created_utc)).total_seconds() < 1800:
         all_nums = re.findall(r"\d*\.\d+|\d+", title)
