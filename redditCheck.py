0c50f83ed6e61518d97fd391e20e7529961fec49
Ian Fraser <icf0001@auburn.edu>
2020-05-30 13:02:52 -0500

    altering for Jacob

--- redditCheck.py
+++ redditCheck.py
@@ -11,13 +11,13 @@ reddit = praw.Reddit(client_id=client_id,
                      user_agent=user_agent)
 client = Client(twilio_sid, twilio_auth)
 subreddit = reddit.subreddit('buildapcsales')
-sought_gpu = ['2070', '2060', '2080']
+sought_gpu = ['1660ti', '1660TI', '2060', '2070']
 
 for submission in subreddit.new(limit=3):
     title = submission.title
     if(title[title.find('[')+1: title.find(']')]) == 'GPU' and any(entry in title for entry in sought_gpu):
         all_nums = re.findall(r"\d*\.\d+|\d+", title)
-        filtered_nums = [entry for entry in all_nums if 250 < float(entry) < 450]
+        filtered_nums = [entry for entry in all_nums if 300 < float(entry) < 450]
         if filtered_nums:
             client.messages.create(to=my_num,
                                    from_='+12056354084',
