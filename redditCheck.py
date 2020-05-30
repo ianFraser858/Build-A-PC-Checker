d66b49dc2c19ddbde87090ca7b49f06c81fb9d75
Ian Fraser <icf0001@auburn.edu>
2020-05-30 13:12:08 -0500

    more testing

--- redditCheck.py
+++ redditCheck.py
@@ -16,9 +16,10 @@ sought_gpu = ['1660ti', '1660TI', '2060', '2070', 'ASUS']
 
 for submission in subreddit.new(limit=3):
     title = submission.title
-    if(title[title.find('[')+1: title.find(']')]) == 'MONITOR' \
+    if(title[title.find('[')+1: title.find(']')]) == 'Monitor' \
             and any(entry in title for entry in sought_gpu)\
             and (datetime.utcnow() - datetime.utcfromtimestamp(submission.created_utc)).total_seconds() < 1800:
+        print('hi Jacob')
         all_nums = re.findall(r"\d*\.\d+|\d+", title)
         filtered_nums = [entry for entry in all_nums if 300 < float(entry) < 450]
         if filtered_nums:
